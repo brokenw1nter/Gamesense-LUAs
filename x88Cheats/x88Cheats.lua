354ca5773bf54c68998a379ec15f049962b7d6ae
brokenw1nter <32863827+brokenw1nter@users.noreply.github.com>
2020-09-24 01:10:28 -0700

    Updated Information

--- x88Cheats/x88Cheats.lua
+++ x88Cheats/x88Cheats.lua
@@ -2,19 +2,23 @@
 	Title: x88Cheats
 	Author: brokenw1nter (Discord: w1nter#4947)
 	Version: 2.0
-	Note: I'm actually dumb and can not for the life of me figure out how to check for the things listed in "Needs Fix"
-		  Also Headshot Only hasn't been implemented yet. I'm still trying to figure out GS's API so give me some time.
-		  If you would like to help with the LUA just let me know, I'm open to anyone helping perfect this LUA :D
-		  Also I don't write LUAs so please tell me if there is a more effecient way of doing all these IF Statements..
+	Note: Started this at the beginning of this year to learn the basics of LUA for another API but now that I'm here,
+		  I thought I would convert it then share it since I want to get some feedback on what I could do to improve my habits in LUA.
+		  I'm also stupidly dumb and can not for the life of me figure out how to check for the things listed in "Bugs".
+		  If you would like to help with this LUA, just let me know as I'm opened to anyone that wants to help perfect this LUA.
+		  Also I don't write LUAs so I don't know much about the API so if there are more efficient ways of doing what I have done, please let me know.
 	Changelog: Rewritten for Gamesense's LUA API
-	Needs Fix: In Lobby because 'paint' isn't called unless you're in a game. If there's another callback let me know please.
-			   NoHands doesn't check for hands transparency (don't know how to check the color picker for transparency)
-			   Chams doesn't change to what cham material is selected (don't know how to check the material combobox)
-			   MPoints doesn't do check for anything (don't know how to check the multi-select)
-	Ideas: Enable ESP when Dead
-		   Make all options an actual button rather than just text that displays
-		   Add Legit and SuperLegit checkbox that loads preset settings and stores the original settings for reverting
-	Credits: kopretinka (Gamesense Forums) - Spectators List
+	Bugs: "HS Only" hasn't been implemented yet due to me not knowing how to get the value of some of the items in the menu.
+		  "In Lobby" currently doesn't work due to paint only being called in game, if there's another way of drawing the GUI please let me know.
+		  "NoHands" doesn't check for Hands transparency, meaning it will display that it is on when you have Hands Chams enabled.
+		  "Chams" doesn't display material being used due to me not knowing how to get the value of an unnamed combobox.
+		  "MPoints" doesn't check or display anything also due to me not knowing how to get the value of the different comboboxes.
+	Plans: Function for Enabling ESP when Dead
+		   Function for loading visual settings to replicate @MasterLooser's
+		   Make the GUI interactive rather than just being a huge indicator.
+		   Making "Legit" and "SuperLegit" load preset settings, mainly to just turn off anything that would count as not Legit.
+	Credits: Aviarita (Gamesense Forums) - Anti-Aim Angles
+			 kopretinka (Gamesense Forums) - Spectators List
 ]]--
 
 --------------------------------------------------------------------------------
